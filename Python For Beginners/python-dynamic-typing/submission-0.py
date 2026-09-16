9a66618d9054197b4ef7c50a5f93c8ff9deb2b74
Justin El-Chibani <145512671+JustinEl-Chibani@users.noreply.github.com>
2026-09-16 11:47:04 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 5
+print(type(variable))
+variable = 5.0
+print(type(variable))
+variable = True
+print(type(variable))
+variable = 'Justin'
+print(type(variable))
+variable = [1,2,3]
+print(type(variable))
